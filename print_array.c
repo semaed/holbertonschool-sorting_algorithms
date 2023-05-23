3bf8f16ae9a8a288eb0bbfbe1927efc17bef35aa
semaed <figsaned@gmail.com>
2023-05-23 11:59:21 +0000

    correct print array struct file

--- print_array.c
+++ print_array.c
@@ -1,4 +1,5 @@
-#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
@@ -6,18 +7,17 @@
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
-
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
